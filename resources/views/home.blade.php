79bfebf6950d14692f9433852e3a6a6514f12a76
omar <omarvazquezgordillo@gmail.com>
2017-05-20 12:33:17 -0500

    ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-12 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">Carga de Archivos</div>
 
@@ -12,35 +12,35 @@
                         <form action="{{ url('filesUpload') }}" method="POST" enctype="multipart/form-data">
                             {{ csrf_field() }}
                             <div class="row">
-                            <div class="lg-12">
-                                <div class="lg-3">
+                           
+                                <div class="col-md-4">
                                     <label for="">IFE</label>
                                     <input type="file" class="form-control" name="IFE" id="IFE">
                                 </div>
-                                <div class="lg-3">
+                                <div class="col-md-4">
                                     <label for="">IFE REVERSO</label>
                                     <input type="file" class="form-control" name="IFEREVERSO" id="IFEREVERSO">
                                 </div>
-                                <div class=" lg-3">
+                                <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="">CURP</label>
                                         <input type="file" class="form-control" name="CURP" id="CURP">
                                     </div>
                                 </div>
-                                 <div class=" lg-3">
+                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="">COMPROBANTE DE DOMICILIO</label>
                                         <input type="file" class="form-control" name="COMPROBANTE" id="COMPROBANTE">
                                     </div>
                                 </div>
                                 
-                                <div class=" lg-3">
+                                <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="">SOLICITUD</label>
                                         <input type="file" class="form-control" name="SOLICITUD" id="SOLICITUD">
                                     </div>
                                 </div>
-                                <div class=" lg-3">
+                                <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="">AVISO</label>
                                         <input type="file" class="form-control" name="AVISO" id="AVISO">
@@ -92,7 +92,7 @@
               
                  		
                                 <button class="btn" type="submit">Enviar</button>
-                            </div>
+                            
                             </div>
                         </form>
                     </div>
