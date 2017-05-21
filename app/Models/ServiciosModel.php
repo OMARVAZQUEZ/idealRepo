b79447838b4eacf393741a7630699c1559098cb0
omar <omarvazquezgordillo@gmail.com>
2017-05-21 11:13:47 -0500

    models

---
+++ app/Models/ServiciosModel.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ServiciosModel extends Model
+{
+    protected $table='servicios';
+//     public function  geturlAttribute($value){
+
+//         return "https://s3.amazonaws.com/".env('AWS_BUCKET')."/".$value;
+//     }
+
+}
