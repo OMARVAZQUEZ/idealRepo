c4453d898080a4acc058f782287eb3a59f138c53
omar <omarvazquezgordillo@gmail.com>
2017-05-21 17:10:42 -0500

    url

--- app/Models/ServiciosModel.php
+++ app/Models/ServiciosModel.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 class ServiciosModel extends Model
 {
     protected $table='servicios';
-//     public function  geturlAttribute($value){
+    public function  geturlAttribute($value){
 
-//         return "https://s3.amazonaws.com/".env('AWS_BUCKET')."/".$value;
-//     }
+        return "https://s3.amazonaws.com/".env('AWS_BUCKET')."/".$value;
+    }
 
 }
