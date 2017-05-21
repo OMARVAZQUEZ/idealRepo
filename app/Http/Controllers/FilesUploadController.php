54d8dc6ff9fa07efd64432d48a08b9aefa6669f3
omar <omarvazquezgordillo@gmail.com>
2017-05-21 00:00:37 -0500

    carbonn date

--- app/Http/Controllers/FilesUploadController.php
+++ app/Http/Controllers/FilesUploadController.php
@@ -8,8 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Models\ServiciosModel;
-use App\Http\Controllers\Carbon;
-
+use Carbon\Carbon;
 class FilesUploadController extends Controller
 {
     /**
