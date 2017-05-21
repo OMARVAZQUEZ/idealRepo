764d5acf8f74e91adf878ec2c24c87c0eeefe622
omar <omarvazquezgordillo@gmail.com>
2017-05-20 23:34:17 -0500

    ok

--- app/Http/Controllers/FilesUploadController.php
+++ app/Http/Controllers/FilesUploadController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Models\ServiciosModel;
+use App\Http\Controllers\Carbon;
 
 class FilesUploadController extends Controller
 {
