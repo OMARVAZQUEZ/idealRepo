d96b0516f570779b8f426722dd0b159712a4c93e
omar <omarvazquezgordillo@gmail.com>
2017-05-26 17:02:34 -0500

    if

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,13 +45,16 @@
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
+                           	 
                         @else
-                         <li>
-                                        <a href="/home">Subir Solicitud</a>
-                                    </li>
-                                     <li>
+                        @if(Auth::user()->id==2)<li>
                                         <a href="solicitudes">Solicitudes</a>
                                     </li>
+                             @endif
+                        			 <li>
+                                        <a href="/home">Subir Solicitud</a>
+                                    </li>
+                                     
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
