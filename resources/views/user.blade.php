67204e751f6a5f648c6dba24a048a7ca7118e159
omar <omarvazquezgordillo@gmail.com>
2017-05-25 23:39:30 -0500

    okoko

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,12 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Menu</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Ideal</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Subir Solicitud</a>
+                    </li>
+                    <li>
+                        <a href="#">Servicios Capturados</a>
+                    </li>
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-12 ">
                 <div class="panel panel-default">
                     <div class="panel-heading">Mis archivos</div>
-
                     <div class="panel-body">
                         <table class="table table-hover">
                             <thead>
