8e7df3d2cfbe0863e5ad69fffcd04214c6cb51ba
omar <omarvazquezgordillo@gmail.com>
2017-05-26 15:17:06 -0500

    sdwsws

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,18 +46,16 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu" role="menu">
-									 <li>
+                         <li>
                                         <a href="/home">Subir Solicitud</a>
                                     </li>
                                      <li>
                                         <a href="solicitudes">Mis Solicitud</a>
                                     </li>
-                                    </ul>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
                                         <a href="{{ route('logout') }}"
