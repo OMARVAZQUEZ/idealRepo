1c117d2c8ee581e37529fbad46ba28948cbabd5c
omar <omarvazquezgordillo@gmail.com>
2017-05-29 15:37:59 -0500

    ok ok

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,34 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Menu</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Ideal</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">Subir Solicitud</a>
-                    </li>
-                    <li>
-                        <a href="#">Servicios Capturados</a>
-                    </li>
-                    
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+
 
     <div class="container-fluid">
         <div class="row">
